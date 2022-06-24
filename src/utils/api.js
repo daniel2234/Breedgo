9d38193edb100a6a2c6c4886aaac2c3ed87f3468
daniel2234 <kwiatkowski.daniel@gmail.com>
2022-06-24 09:49:26 -0400

    setting up image api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,31 @@
 import Dog from '../models/Dog';
 
-export function fetchDogBreeds(type) {
+export function fetchDogBreeds(type = "breeds") {
   //type will be a fill for this api method
-  return fetch(`https://dog.ceo/api/breeds/list/all`)
+  return fetch(`https://dog.ceo/api/${type}/list/all`)
   .then(response => response.json())
   .then(data => {
-    dogs = Object.keys(data.message)
-    subBreeds = Object.values(data.message)
-    return makeDogObjects(dogs, subBreeds)
+    // dogs = Object.keys(data.message)
+    // subBreeds = Object.values(data.message)
+    // return makeDogObjects(dogs, subBreeds)
 
     //original object data.message
+    console.log(data.message)
+    return data.message
   });
 }
+
+
+export function fetchRandomDogImages(type='affenpinscher') {
+  return fetch(`https://dog.ceo/api/breed/${type}/images`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+  });
+}
+
+
+
 
 let dogs = []
 let subBreeds = []
@@ -37,3 +51,21 @@ function makeDogObjects(breeds, subBreeds) {
   console.log(arrayOfDogs);
   return arrayOfDogs
 }
+
+function randomPhoto(min, max) {
+  console.log( Math.floor(Math.random() * (max - min + 1)) + min);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+//there is a variable amount of random photos from this image api per breed
+//for this fetch request we will have to generate a random number between 0-1000
+//photos are in order, created a random Int function for grab random photos
+//the max value in the randomInt function will adapt for each image api call.
+
+fetch(`https://dog.ceo/api/breed/affenpinscher/images`)
+    .then(response => response.json())
+    .then(data => {
+     let randomInt = randomPhoto(0, data.message.length);
+      console.log(data.message)
+ });
