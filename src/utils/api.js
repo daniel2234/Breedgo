bf8e3047fe5a0dc6926207131739588adf04a4d2
daniel2234 <kwiatkowski.daniel@gmail.com>
2022-06-24 13:03:25 -0400

    added api functionality for random images

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,26 +1,26 @@
 import Dog from '../models/Dog';
 
-export function fetchDogBreeds(type = "breeds") {
-  //type will be a fill for this api method
-  return fetch(`https://dog.ceo/api/${type}/list/all`)
-  .then(response => response.json())
-  .then(data => {
-    // dogs = Object.keys(data.message)
-    // subBreeds = Object.values(data.message)
-    // return makeDogObjects(dogs, subBreeds)
+// export function fetchDogBreeds(type = "breeds") {
+//   //type will be a fill for this api method
+//   return fetch(`https://dog.ceo/api/${type}/list/all`)
+//   .then(response => response.json())
+//   .then(data => {
+//     // dogs = Object.keys(data.message)
+//     // subBreeds = Object.values(data.message)
+//     // return makeDogObjects(dogs, subBreeds)
 
-    //original object data.message
-    console.log(data.message)
-    return data.message
-  });
-}
+//     //original object data.message
+//     console.log(data.message)
+//     return data.message
+//   });
+// }
 
 
 export function fetchRandomDogImages(type='affenpinscher') {
   return fetch(`https://dog.ceo/api/breed/${type}/images`)
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      console.log(data.message, 'here it is called.')
   });
 }
 
@@ -48,7 +48,6 @@ function makeDogObjects(breeds, subBreeds) {
     let dog = new Dog(i, breeds[i], subBreeds[i]);
     arrayOfDogs.push(dog);
   }
-  console.log(arrayOfDogs);
   return arrayOfDogs
 }
 
@@ -56,7 +55,6 @@ function randomPhoto(min, max) {
   console.log( Math.floor(Math.random() * (max - min + 1)) + min);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 
 //there is a variable amount of random photos from this image api per breed
 //for this fetch request we will have to generate a random number between 0-1000
@@ -66,6 +64,7 @@ function randomPhoto(min, max) {
 fetch(`https://dog.ceo/api/breed/affenpinscher/images`)
     .then(response => response.json())
     .then(data => {
-     let randomInt = randomPhoto(0, data.message.length);
-      console.log(data.message)
+      console.log(data.message, 'here it is called')
+      let randomImage = randomPhoto(0, data.message.length);
+      console.log(randomImage);
  });
